8559fe164845e458df7e4e6072764cb8371ed464
Sivam Pasupathipillai <sivam.pasupathipillai@gmail.com>
2024-08-03 17:17:54 +0200

    fix: add homepage draft

---
+++ src/components/AppGallery.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app-container">
+    <a href="/snake">
+      <div class="app-card">
+        <div>SNAKE</div>
+      </div>
+    </a>
+    <div class="app-card">
+      <div>PYTHON</div>
+    </div>
+    <div class="app-card">
+      <div>MACHINE LEARNING</div>
+    </div>
+    <div class="app-card">
+      <div>LOREM IPSUM</div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app-container a {
+  text-decoration: none;
+}
+
+#app-container {
+  column-gap: var(--general-box-padding);
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  padding: var(--general-box-padding);
+  row-gap: var(--general-box-padding);
+}
+
+.app-card {
+  align-items: center;
+  background-color: var(--app-card-bg-color);
+  border: 0.15rem solid var(--app-card-border-color);
+  border-radius: 1rem;
+  box-shadow: 5px 5px 5px var(--app-card-shadow-color);
+  color: var(--app-card-color);
+  display: flex;
+  font-size: var(--app-card-font-size);
+  font-style: bold;
+  height: 200px;
+  justify-content: space-around;
+  width: 200px;
+}
+
+.app-card:hover {
+  border: 0.15rem solid var(--app-card-hover-border-color);
+  box-shadow: 5px 5px 5px grey;
+}
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,56 @@
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
+    <div class="main-contents">
+      <h1>Sivam Pasupathipillai, Ph.D.</h1>
+      <hr />
+      <h2>a personal website</h2>
+      <AppGallery />
+    </div>
   </div>
 </template>
+
+<style>
+/* https://colorhunt.co/palette/def9c49cdba650b498468585 */
+:root {
+  --app-card-bg-color: #468585;
+  --app-card-border-color: #50B498;
+  --app-card-color: #DEF9C4;
+  --app-card-font-size: 1rem;
+  --app-card-hover-border-color: #9CDBA6;
+  --app-card-shadow-color: grey;
+  --general-box-padding: 1.5rem;
+  --main-contents-bg-color: #DEF9C4;
+  --subtitle-color: #50B498;
+  --title-color: #468585;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.main-contents {
+  background-color: var(--main-contents-bg-color);
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  min-height: 100vh;
+  width: 700px;
+}
+
+.main-contents h1,
+.main-contents h2 {
+  margin: 0;
+}
+.main-contents h1 {
+  color: var(--title-color);
+  padding: var(--general-box-padding);
+  padding-bottom: 0;
+}
+
+.main-contents h2 {
+  color: var(--subtitle-color);
+  padding-right: var(--general-box-padding);
+  text-align: right;
+}
+</style>
